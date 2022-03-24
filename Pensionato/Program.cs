a90ed89c9ec023cd3d57346efca6b4044b09cf7c
Yanny Dórea Moscovits <49878332+yannymoscovits@users.noreply.github.com>
2022-03-24 02:11:57 -0300

    Adicionar arquivos de projeto.

---
+++ Pensionato/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Quartos[] vect = new Quartos[10];
+         
+            Console.WriteLine("How many rooms will be rented? :");
+            int n = int.Parse(Console.ReadLine());
+
+            
+
+         for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(" RENT #"+i+" :"     );
+                
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Email :");
+                string email = Console.ReadLine();
+                Console.WriteLine("Room :");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Quartos(name, email);
+            }
+
+            Console.WriteLine("Busy rooms : ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null) {
+                    Console.WriteLine( i + ":" + vect[i]);
+                }
+               
+            }
+         
+        }
+    }
+}
---
+++ Pensionato/Quartos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensionato
+{
+    internal class Quartos
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+        
+        public  Quartos (string nome, string email)
+        {
+            this.Name = nome;
+            this.Email = email;
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " , " + this.Email;
+        }
+
+    }
+}
